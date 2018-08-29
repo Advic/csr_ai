245f71079bd068e5bb53e36b54919f0b1ebbe0d9
mho <loktinmartinho@gmail.com>
2018-08-29 00:07:11 -0700

    Add __str__ to spice.py, add test

--- game/spice.py
+++ game/spice.py
@@ -1,5 +1,8 @@
 from collections import namedtuple
 from enum import Enum
+from itertools import chain, repeat
+
+from colored import fg, attr
 
 
 class Spice(Enum):
@@ -11,6 +14,10 @@ class Spice(Enum):
 
 
 class SpiceSet(namedtuple('SpiceBase', ('turmeric', 'saffron', 'cardamom', 'cinnamon'))):
+    # Colors for the 4 spices, in the same order as the namedtuple definition.
+    # turmeric: yellow_1, saffron: red, cardamom: chartreuse_2b, cinnamon: dark_orange_3a
+    _colors = [fg('yellow_1'), fg('red'), fg('chartreuse_2b'), fg('dark_orange_3a')]
+
     def __init__(self, *args, **kwargs):
         super(SpiceSet, self).__init__(**kwargs)
 
@@ -31,3 +38,16 @@ class SpiceSet(namedtuple('SpiceBase', ('turmeric', 'saffron', 'cardamom', 'cinn
     @property
     def size(self):
         return self.turmeric + self.saffron + self.cardamom + self.cinnamon
+
+    def __str__(self):
+        """
+
+        Returns: unicode string with ANSI color code, colored as the actual board game version is made
+
+        """
+
+        def colored_square(color):
+            return color + chr(0x25A0) + attr('reset')
+
+        return ' '.join(reversed(
+            list(chain.from_iterable(repeat(colored_square(clr), qty) for qty, clr in zip(self, self._colors)))))
--- tests/test_spice.py
+++ tests/test_spice.py
@@ -9,6 +9,15 @@ def test_spiceset():
     assert ss.cinnamon == 4
 
 
+def test_spiceset_str():
+    assert str(spice.SpiceSet(0, 0, 0, 1)) == "\x1b[38;5;130m\u25A0\x1b[0m"  # Cinnamon: brown square
+    assert str(spice.SpiceSet(0, 0, 1, 0)) == "\x1b[38;5;112m\u25A0\x1b[0m"  # Cardamom: green square
+    assert str(spice.SpiceSet(0, 1, 0, 0)) == "\x1b[38;5;1m\u25A0\x1b[0m"  # Saffron: red square
+    assert str(spice.SpiceSet(1, 0, 0, 0)) == "\x1b[38;5;226m\u25A0\x1b[0m"  # Turmeric: yellow square
+    y_g_b = "\x1b[38;5;130m\u25A0\x1b[0m \x1b[38;5;112m\u25A0\x1b[0m \x1b[38;5;226m\u25A0\x1b[0m"
+    assert str(spice.SpiceSet(1, 0, 1, 1)) == y_g_b
+
+
 def test_spiceset_sum():
     ss = spice.SpiceSet(1, 2, 3, 4)
     assert sum(ss) == 10
