58a38d53fbc79568663a955b71855e9b0073789c
mho <loktinmartinho@gmail.com>
2018-09-09 00:05:59 -0700

    Add utf-8 coding to files

--- game/main.py
+++ game/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import curses
 from collections import Iterable
 from random import shuffle
 
@@ -38,3 +41,36 @@ class Game:
 
     def initialize_players(self):
         pass
+
+
+def main(stdscr):
+    # Clear screen
+    stdscr.clear()
+    win = curses.newwin(51, 215, 0, 0)
+
+    # Initialize colors corresponding to spice colors
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
+    curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    assert curses.has_colors()
+    win.addstr(0, 0, "\u25a3", curses.color_pair(1))
+    win.addstr(0, 1, "\u25a3", curses.color_pair(2))
+    win.addstr(0, 2, "\u25a3", curses.color_pair(3))
+    win.addstr(0, 3, "\u25a3", curses.color_pair(4))
+
+    win.refresh()
+    win.getkey()
+
+
+def render_game():
+    pass
+
+
+def get_action():
+    pass
+
+
+if __name__ == "__main__":
+    curses.wrapper(main)
--- game/spice.py
+++ game/spice.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from collections import namedtuple
 from enum import Enum
 from itertools import chain, repeat
